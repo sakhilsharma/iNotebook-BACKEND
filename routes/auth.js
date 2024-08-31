721e83a75bd71b2bbbb064f21aa95fc57548ef03
Sakhil Sharma <sakhilsharma123@gmial.com>
2024-08-31 21:58:20 +0530

    bcrypt and hash

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();//modular, mountable route handlers:helps to handle routes 
 const User = require('../models/User.js');
 const { body, validationResult } = require('express-validator');
-
+const bcrypt = require('bcrypt');
 //Create a  user using :POST "/api/auth/createuser"
+
+
 router.post('/createuser', [
     body('email', 'enter a valid email').isEmail(),
     body('name', 'enter a valid name').isLength({ min: 3 }),
@@ -25,18 +27,26 @@ router.post('/createuser', [
             if (user) {
                 return res.status(400).json({ error: "Sorry a user with this email already exist" })
             }
+
+            //password hashing
+            const salt = bcrypt.genSalt(10);
+            const secpass = await bcrypt.hash(req.body.password, 10);
+
+
             //if uer doesn't exist then create user
             user = await User.create({
                 name: req.body.name,
-                password: req.body.password,
+                password: secpass,
                 email: req.body.email,
             })
             /*.then(user => res.json(user))
             .catch(err=> {res.json('please enter a unique value');
                 console.log(err);//will throw err
             })*/
+
             res.json(user);
-        }catch (error) { console.log(error.message) ;
+        } catch (error) {
+            console.log(error.message);
             res.status(500).send("Some error occured");
         }
 
